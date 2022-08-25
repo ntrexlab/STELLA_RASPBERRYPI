1444c0b0e3a659da722f8c53b078ea8343bdf227
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2022-08-25 16:20:30 +0900

    Update mobilerobot.cpp

--- stella/stella_md/src/mobilerobot.cpp
+++ stella/stella_md/src/mobilerobot.cpp
@@ -4,8 +4,8 @@ void calculate_wheel_vel(float linear, float angular, float *left_rpm,  float *r
 {
     float left_speed = 0.0, right_speed = 0.0;
 
-    left_speed =  (linear + wheel_to_wheel_d / 2.0 * angular) * motor_scale_const;
-    right_speed = (linear - wheel_to_wheel_d / 2.0 * angular) * motor_scale_const;
+    left_speed =  (linear - wheel_to_wheel_d / 2.0 * angular) * motor_scale_const;
+    right_speed = (linear + wheel_to_wheel_d / 2.0 * angular) * motor_scale_const;
 
     *left_rpm  = left_speed * -1;
     *right_rpm = right_speed * -1;
